caacf272d459c4c8205e9c8d17048245aae6742f
NG-MinG <wgmin.it@outlook.com>
2023-12-25 00:55:35 +0700

    comment video hightlight

--- welegend/app/(pages)/home/page.tsx
+++ welegend/app/(pages)/home/page.tsx
@@ -21,7 +21,7 @@ export default function HomePage() {
       >
         <ProminentStory />
         <Motto />
-        <VideoHighlight />
+        {/* <VideoHighlight /> */}
         <ProminentBlog />
         <BuildLifeSkill />
         <ChoiceWelegnd />
