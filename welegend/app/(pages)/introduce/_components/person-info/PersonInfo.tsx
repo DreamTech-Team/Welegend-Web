5c8087f2e567bf5f9d1371160a3b2e314b3fe085
Bao <20127557@student.hcmus.edu.vn>
2024-02-25 15:44:48 +0700

    BL: feat: update team builds

---
+++ welegend/app/(pages)/introduce/_components/person-info/PersonInfo.tsx
@@ -0,0 +1,24 @@
+import Image from 'next/image';
+import { profile_image2 } from '~/app/_externals/assets/introduce';
+
+export function PersonInfo() {
+  return (
+    <div className="text-center w-fit">
+      <div className="h-32 w-32 sm:h-32 sm:w-32 md:h-36 md:w-36 lg:h-40 lg:w-40">
+        <Image
+          className="rounded-[100%] h-[100%] w-[100%]"
+          src={profile_image2.src}
+          width={250}
+          height={250}
+          alt="no image"
+        />
+      </div>
+      <h1 className="text-[14px] md:text-[20px] font-[600] leading-4 mt-5">
+        Lalisa Manobal
+      </h1>
+      <p className="m-0 text-[10px] md:text-[16px] font-[500] text-neutral-400 ">
+        Rapper
+      </p>
+    </div>
+  );
+}
--- welegend/app/_externals/assets/introduce/index.ts
+++ welegend/app/_externals/assets/introduce/index.ts
@@ -1,14 +1,15 @@
 import banner_intro from './banner_intro.jpg';
 import background_image from './bg.png';
-import no_image from './none_image.png';
-import rainbow_image from './rainbow_image.png';
-import wave from './wave.svg';
-import whale_image from './whale_image.png';
-import childrenbg_image from './childrenbg.webp';
 import children_image from './children_st.png';
+import childrenbg_image from './childrenbg.webp';
+import no_image from './none_image.png';
+import profile_image from './profile_image.jpg';
+import profile_image2 from './profile_image2.jpg';
+import rainbow_image from './rainbow_image.png';
 import story_bg from './story_bg.webp';
 import story_img from './story_img1.webp';
-import profile_image from './profile_image.jpg';
+import wave from './wave.svg';
+import whale_image from './whale_image.png';
 
 export {
   banner_intro,
@@ -22,4 +23,5 @@ export {
   story_bg,
   story_img,
   profile_image,
+  profile_image2,
 };
