16690ce74d91b4691eb2a3ce84a0a402536a1ade
Bao <20127557@student.hcmus.edu.vn>
2024-03-02 23:28:11 +0700

    BL: fix: background when contact form open

--- welegend/app/_externals/components/popup-mesage/PopupMessage.tsx
+++ welegend/app/_externals/components/popup-mesage/PopupMessage.tsx
@@ -17,34 +17,39 @@ export function PopupMessage() {
   };
 
   return (
-    <div className="fixed bottom-10 right-10 z-999">
-      {initialized && (
-        <div
-          className={`absolute bottom-[-5px] right-[-5px] z-998 ${
-            open ? 'scale-up-br' : 'scale-out-br'
-          }`}
-        >
-          <ContactForm />
-        </div>
+    <div className="">
+      {open && (
+        <div className="absolute w-screen h-screen top-0 left-0 opacity-40 bg-black z-998"></div>
       )}
-      <div
-        className="relative z-999 h-[4.2rem] w-[4.2rem] p-[0.4rem] bg-sky-500 rounded-full"
-        onClick={handleClick}
-      >
-        <div className="heartbeat flex justify-center items-center h-[100%] w-[100%] bg-white rounded-full hover:cursor-pointer">
-          {open ? (
-            <FontAwesomeIcon
-              className="rotate-center text-sky-800"
-              icon={faXmark}
-              size="2x"
-            />
-          ) : (
-            <FontAwesomeIcon
-              className="text-sky-800"
-              icon={faCommentDots}
-              size="2x"
-            />
-          )}
+      <div className="fixed bottom-10 right-10 z-999">
+        {initialized && (
+          <div
+            className={`absolute bottom-[-5px] right-[-5px] z-998 ${
+              open ? 'scale-up-br' : 'scale-out-br'
+            }`}
+          >
+            <ContactForm />
+          </div>
+        )}
+        <div
+          className="relative z-999 h-[4.2rem] w-[4.2rem] p-[0.4rem] bg-sky-500 rounded-full"
+          onClick={handleClick}
+        >
+          <div className="heartbeat flex justify-center items-center h-[100%] w-[100%] bg-white rounded-full hover:cursor-pointer">
+            {open ? (
+              <FontAwesomeIcon
+                className="rotate-center text-sky-800"
+                icon={faXmark}
+                size="2x"
+              />
+            ) : (
+              <FontAwesomeIcon
+                className="text-sky-800"
+                icon={faCommentDots}
+                size="2x"
+              />
+            )}
+          </div>
         </div>
       </div>
     </div>
