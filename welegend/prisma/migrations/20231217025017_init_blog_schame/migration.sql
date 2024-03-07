2ef7f37e83b5d23f42ec15ffe0fa0f561961c0e5
AndyKoi <anhkhoi1192002@gmail.com>
2024-03-07 16:12:26 +0700

    Khoi: update APIs blogs

--- welegend/prisma/migrations/20231217025017_init_blog_schame/migration.sql
+++ welegend/prisma/migrations/20231217025017_init_blog_schame/migration.sql
@@ -1,12 +1,34 @@
 -- CreateTable
+-- UserRole
+CREATE TYPE UserRole AS ENUM ('ADMIN', 'PARENT', 'CHILDREN');
+
+-- User
+CREATE TABLE "User" (
+    "id" UUID NOT NULL DEFAULT uuid_generate_v4(),
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL UNIQUE,
+    "password" TEXT NOT NULL,
+    "phone" TEXT DEFAULT '',
+    "image" TEXT DEFAULT '',
+    "address" TEXT,
+    "role" UserRole DEFAULT 'PARENT',
+    "createdAt" TIMESTAMP NOT NULL DEFAULT current_timestamp,
+    "updatedAt" TIMESTAMP NOT NULL DEFAULT current_timestamp,
+    "birthdate" TIMESTAMP,
+    "gender" TEXT DEFAULT 'Male',
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- Blog
 CREATE TABLE "Blog" (
     "id" SERIAL NOT NULL,
-    "author" TEXT NOT NULL,
+    "authorId" UUID,
     "title" TEXT NOT NULL,
-    "summary" TEXT NOT NULL,
+    "summary" TEXT,
     "content" TEXT NOT NULL,
-    "avatar" TEXT,
-    "create_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "Blog_pkey" PRIMARY KEY ("id")
+    "thumbnail" TEXT,
+    "create_at" TIMESTAMP NOT NULL DEFAULT current_timestamp,
+    CONSTRAINT "Blog_pkey" PRIMARY KEY ("id"),
+    CONSTRAINT "FK_Blog_authorId_User_id" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE SET NULL
 );
+
